323ac953fe7d24f63b57c6af291debc43f87f863
Dmitry Rakhalov <biorobot@gmail.com>
2017-12-10 12:46:19 +0300

    Small optimizations

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,23 @@
 from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
-import os, shutil, glob
+import shutil
 
 class GoogleBenchmarkConan(ConanFile):
     name = 'benchmark'
     version = '1.3.0'
     description = 'A microbenchmark support library.'
     url = 'http://github.com/rt12/conan-google-benchmark'
-    license = 'https://github.com/google/benchmark/blob/v1.1.0/LICENSE'
+    license = 'https://github.com/google/benchmark/blob/v1.3.0/LICENSE'
     settings = 'arch', 'build_type', 'compiler', 'os'
     options = { 
 	'enable_lto': [True, False], 
         'enable_exceptions': [True, False],
 	}
-    default_options = """
-enable_lto=False
-enable_exceptions=True
-    """
+    default_options = "enable_lto=False", "enable_exceptions=True"
     generators = 'cmake'
 
     def source(self):
-        archive_url = 'https://github.com/google/benchmark/archive/v{!s}.zip'.format(self.version)
-        tools.download(archive_url, 'benchmark.zip')
-        # tools.check_sha256('benchmark.zip', '3f5321836cf531e621e0187ccbb1d836cd909994ed00c102a41385cbc1254e4e')
-        tools.unzip('benchmark.zip')
-        os.unlink('benchmark.zip')
+        archive_url = 'https://github.com/google/benchmark/archive/v{!s}.tar.gz'.format(self.version)
+        tools.get(archive_url, sha256='f19559475a592cbd5ac48b61f6b9cedf87f0b6775d1443de54cfe8f53940b28d')
         shutil.move('benchmark-{!s}'.format(self.version), 'benchmark')
 
     def build(self):
@@ -34,11 +27,11 @@ enable_exceptions=True
             activated = getattr(self.options, option_name)
             cmake.definitions['BENCHMARK_' + option_name.upper()] = 'ON' if activated else 'OFF'
         cmake.definitions['BENCHMARK_USE_LIBCXX'] = 'ON' if self.settings.compiler.libcxx else 'OFF'
-        print (cmake.command_line)
         cmake.install()
 
     def package(self):
-        print("Packaging...")
+        # already packaged by cmake.install()
+        pass
 
     def package_info(self):
         # let consuming projects know what library name is used for linking
